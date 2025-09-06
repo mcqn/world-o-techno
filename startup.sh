3a399e24a016f854151dc09dcb7924eb8cfe9473
Adrian McEwen <adrianm@mcqn.com>
2025-09-06 22:27:07 +0100

    Tweaks for the RPi 2, which is slower to start sonic-pi (and seemed to need a different volume setup)

--- startup.sh
+++ startup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -x
 
 # wait for sonic-pi to start
-sleep 20
+# (a RPi 2 is very slow)
+sleep 65
 
 # set the default audio output to be the headphone jack
 amixer cset numid=3 1
 
 # set audio volume to full
 amixer sset PCM 100%
+
+# and Master volume to ~50%
+amixer set "Master" 46444,46444
 
 # play our tune
 cat /home/pi/world-o-techno/world-o-techno.rb|sonic_pi
